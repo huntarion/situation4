7c5ac31a3f2727b8281bde4ec78b958c1a196909
huntarion <oxwoos@gmail.com>
2016-01-28 20:07:18 +0600

    Восстановление данных после реинсталяции оси №2

---
+++ front-end/js/main5.js
@@ -0,0 +1,26 @@
+/* var // примитивы
+str = 'YPA!', 
+number = 1,
+numberSecond = 2.4,
+bool = true,
+undef,
+nulll = null;
+
+var obj ={ //объектные
+	prop: "yo",
+	fu: function () {
+	}
+}
+
+regexp = /baba/g,
+func = function() {},
+arr = [1,2,3];
+
+obj.prop = 'lorooolo';
+arr[2] = 24;
+
+
+// обёртки */
+
+
+
